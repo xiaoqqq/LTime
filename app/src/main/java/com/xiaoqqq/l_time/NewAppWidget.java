4719f9d1bee263e05bf50659995dd9545dc423ed
xiaoqqq <magic_xiao@126.com>
2019-07-08 17:03:52 +0800

    30min update

--- app/src/main/java/com/xiaoqqq/l_time/NewAppWidget.java
+++ app/src/main/java/com/xiaoqqq/l_time/NewAppWidget.java
@@ -3,8 +3,8 @@ package com.xiaoqqq.l_time;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.os.Handler;
-import android.os.Message;
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import com.xiaoqqq.l_time.utils.DateUtils;
@@ -19,43 +19,35 @@ import java.util.Date;
  */
 public class NewAppWidget extends AppWidgetProvider {
 
-    private AppWidgetManager mAppWidgetManager;
-    private Context mContext;
-    private int appWidgetId;
+    void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        String startTime = "2019-03-23";
+        DateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date startDate = simpleDateFormat.parse(startTime);
+            Date currentDate = simpleDateFormat.parse(simpleDateFormat.format(new Date()));
+            String widgetString = "恋爱第 " + DateUtils.getDaysByDate(startDate, currentDate) + " 天";
+            views.setTextViewText(R.id.appwidget_text, widgetString);
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
-    private Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(final Message msg) {
-            super.handleMessage(msg);
-            if (msg.what == 1) {
-                // Construct the RemoteViews object
-                RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.new_app_widget);
-                String startTime = "2019-03-23";
-                DateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                try {
-                    Date startDate = simpleDateFormat.parse(startTime);
-                    Date currentDate = simpleDateFormat.parse(simpleDateFormat.format(new Date()));
-                    String widgetString = "恋爱第 " + DateUtils.getDaysByDate(startDate, currentDate) + " 天";
-                    views.setTextViewText(R.id.appwidget_text, widgetString);
-                    // Instruct the widget manager to update the widget
-                    mAppWidgetManager.updateAppWidget(appWidgetId, views);
-                } catch (ParseException e) {
-                    e.printStackTrace();
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        super.onReceive(context, intent);
+        String action = intent.getAction();
+        if ("android.intent.action.SCREEN_ON".equals(action)) {
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                int[] appWidgetIds = extras.getIntArray(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+                if (appWidgetIds != null && appWidgetIds.length > 0) {
+                    this.onUpdate(context, AppWidgetManager.getInstance(context), appWidgetIds);
                 }
             }
         }
-    };
-
-    private Thread mTimerThread;
-
-    void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                         int appWidgetId) {
-        this.mAppWidgetManager = appWidgetManager;
-        this.mContext = context;
-        this.appWidgetId = appWidgetId;
-        mTimerThread = new Thread(new TimerThread());
-        mTimerThread.start();
-
     }
 
     @Override
@@ -76,22 +68,5 @@ public class NewAppWidget extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    class TimerThread extends Thread {//这里也可用Runnable接口实现
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    Thread.sleep(1000);//每隔1s执行一次
-                    Message msg = new Message();
-                    msg.what = 1;
-                    mHandler.sendMessage(msg);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-    }
 }
 
